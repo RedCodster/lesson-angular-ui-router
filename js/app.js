09bd1be682bf521f9897df820da39ddcee9d5287
Robert <rob_fong00@yahoo.com>
2016-03-22 15:18:20 +0800

    final

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 angular
-  .module('TodosApp', []);
+  .module('TodosApp', ['ui.router'])
+  .config(MainRouter);
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home',{
+      url:'/',
+      templateUrl:'home.html'
+    })
+    .state('archive', {
+      url: "/archive",
+      templateUrl: "archive.html"
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
